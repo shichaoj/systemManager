d7842d6cd99d69c249e923ab27b8df4aaaa39bf6
shichaoj <504315264@qq.com>
2017-03-06 16:39:33 +0800

    0306

---
+++ Bmob_IM_V1.1.9/IM_Demo/src/com/bmob/im/demo/config/Config.java
@@ -0,0 +1,13 @@
+package com.bmob.im.demo.config;
+
+/** 系统变量
+  * @ClassName: Config
+  * @Description: TODO
+  * @author smile
+  * @date 2014-6-17 上午9:40:11
+  */
+public class Config {
+
+	//这是Bmob的ApplicationId,用于初始化操作
+	public static String applicationId = "339844e9602c7d7c891fcf99ae77a05f";
+}
